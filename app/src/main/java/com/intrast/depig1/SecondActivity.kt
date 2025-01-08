4ed1b0fc9673014b86bc0e512c0395d90a534030
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2025-01-08 20:49:16 +0200

    add 2 fragments statically

---
+++ app/src/main/java/com/intrast/depig1/SecondActivity.kt
@@ -0,0 +1,38 @@
+package com.intrast.depig1
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.intrast.depig1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initComponent()
+    }
+
+    private fun initComponent() {
+        // dynamically assign the fragments to the activity
+        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            supportFragmentManager.beginTransaction().replace(R.id.container2,BottomFragment()).commit()
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.container, TopFragment()).commit()
+    }
+
+    fun getData(name: String) {
+    val bottomFragment = BottomFragment.newInstance(name)
+        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            supportFragmentManager.beginTransaction().replace(R.id.container2, bottomFragment)
+                .commit()
+        }
+
+    }
+}
--- app/src/main/java/com/intrast/depig1/TopFragment.kt
+++ app/src/main/java/com/intrast/depig1/TopFragment.kt
@@ -7,16 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.intrast.depig1.databinding.FragmentTopBinding
 
-//// TODO: Rename parameter arguments, choose names that match
-//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
 
-///**
-// * A simple [Fragment] subclass.
-// * Use the [TopFragment.newInstance] factory method to
-// * create an instance of this fragment.
-// */
 class TopFragment : Fragment() {
     lateinit var binding: FragmentTopBinding
     override fun onCreateView(
@@ -32,43 +23,15 @@ class TopFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.submit.setOnClickListener {
             val name = binding.editText.text.toString()
-            (activity as MainActivity).getData(name)
+            if (activity is MainActivity){
+                (activity as MainActivity).getData(name)
+            }else if(activity is SecondActivity){
+                (activity as SecondActivity).getData(name)
+            }
+
             binding.editText.text.clear()
         }
     }
 
 
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
-
-
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment TopFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            TopFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
