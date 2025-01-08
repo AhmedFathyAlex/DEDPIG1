58c908bd8ee7383da29308d476106aae0d252d21
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2025-01-08 19:24:44 +0200

    first commit

---
+++ app/src/main/java/com/intrast/depig1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.intrast.depig1
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.intrast.depig1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        }
+
+    fun getData(name:String){
+        val bottomFragment = supportFragmentManager.findFragmentById(R.id.bottomFragment) as BottomFragment
+            bottomFragment.updateText(name)
+
+    }
+
+}
